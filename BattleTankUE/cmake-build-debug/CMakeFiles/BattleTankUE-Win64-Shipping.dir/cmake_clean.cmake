cb33addd3aa9a750f93dd1dd8f537b7aa78783bd
Samdan D <pri.samdan@gmail.com>
2019-06-02 22:51:00 +0800

    Deleted IDEs config file other than VS 2019
    Former-commit-id: a00e3e1779320ce61934fc299af2b1dbd280aecc

--- BattleTankUE/cmake-build-debug/CMakeFiles/BattleTankUE-Win64-Shipping.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/BattleTankUE-Win64-Shipping"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BattleTankUE-Win64-Shipping.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- BattleTankUE/cmake-build-debug/CMakeFiles/BattleTankUE.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/BattleTankUE"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BattleTankUE.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
